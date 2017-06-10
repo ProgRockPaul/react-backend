68dbe5f54d0c654290e9f2015087ec945460988f
Paul Walsh <twentytwentyk@live.com>
2017-06-09 22:58:41 -0400

    added a for loop for the same thing

--- client/src/Api.jsx
+++ client/src/Api.jsx
@@ -33,28 +33,22 @@ class Api extends Component {
         childArray.push(child[i].data);
       }
        // these are the children or the individual posts on Reddit
-      var url1 = "https://www.reddit.com" + childArray[0].permalink.toString();
-      var url2 = "https://www.reddit.com" + childArray[1].permalink.toString();
-      var url3 = "https://www.reddit.com" + childArray[2].permalink.toString();
-      var url4 = "https://www.reddit.com" + childArray[3].permalink.toString();
-      var url5 = "https://www.reddit.com" + childArray[4].permalink.toString();
-      var url6 = "https://www.reddit.com" + childArray[5].permalink.toString();
-      var url7 = "https://www.reddit.com" + childArray[6].permalink.toString();
-      var url8 = "https://www.reddit.com" + childArray[7].permalink.toString();
-      var url9 = "https://www.reddit.com" + childArray[8].permalink.toString();
-      var url10 = "https://www.reddit.com" + childArray[9].permalink.toString();
+      var permaUrl = [];
+      for(var i = 0; i < childArray.length; i++){
+        permaUrl.push("https://www.reddit.com" + childArray[0].permalink.toString());
+      }
+      console.log([permaUrl])
 
-
-      var r1 = <i className="fa fa-thumbs-o-up">1: <a href= {url1} >{childArray[0].title}</a> {childArray[0].ups} upvotes </i>;
-        var r2 = <i className="fa fa-thumbs-o-up">2: <a href= {url2}>{childArray[1].title}</a> {childArray[1].ups} upvotes</i>;
-          var r3 = <i className="fa fa-thumbs-o-up">3: <a href= {url3}>{childArray[2].title}</a>  {childArray[2].ups} upvotes</i>;
-            var r4 = <i className="fa fa-thumbs-o-up">4: <a href= {url4}>{childArray[3].title}</a> {childArray[3].ups} upvotes</i>;
-              var r5 = <i className="fa fa-thumbs-o-up">5: <a href= {url5}>{childArray[4].title}</a> {childArray[4].ups} upvotes</i>;
-                var r6 = <i className="fa fa-thumbs-o-up">6: <a href= {url6}>{childArray[5].title}</a> {childArray[5].ups} upvotes</i>;
-                  var r7 = <i className="fa fa-thumbs-o-up">7: <a href= {url7}>{childArray[6].title}</a> {childArray[6].ups} upvotes</i>;
-                    var r8 = <i className="fa fa-thumbs-o-up">8: <a href= {url8}>{childArray[7].title}</a> {childArray[7].ups} upvotes</i>;
-                      var r9 = <i className="fa fa-thumbs-o-up">9: <a href= {url9}>{childArray[8].title}</a> {childArray[8].ups} upvotes</i>;
-                        var r10 = <i className="fa fa-thumbs-o-up">10: <a href= {url10}>{childArray[9].title}</a> {childArray[9].ups} upvotes</i>;
+      var r1 = <i className="fa fa-thumbs-o-up">1: <a href= {permaUrl[0]} >{childArray[0].title}</a> {childArray[0].ups} upvotes </i>;
+        var r2 = <i className="fa fa-thumbs-o-up">2: <a href= {permaUrl[0]}>{childArray[1].title}</a> {childArray[1].ups} upvotes</i>;
+          var r3 = <i className="fa fa-thumbs-o-up">3: <a href= {permaUrl[0]}>{childArray[2].title}</a>  {childArray[2].ups} upvotes</i>;
+            var r4 = <i className="fa fa-thumbs-o-up">4: <a href= {permaUrl[0]}>{childArray[3].title}</a> {childArray[3].ups} upvotes</i>;
+              var r5 = <i className="fa fa-thumbs-o-up">5: <a href= {permaUrl[0]}>{childArray[4].title}</a> {childArray[4].ups} upvotes</i>;
+                var r6 = <i className="fa fa-thumbs-o-up">6: <a href= {permaUrl[0]}>{childArray[5].title}</a> {childArray[5].ups} upvotes</i>;
+                  var r7 = <i className="fa fa-thumbs-o-up">7: <a href= {permaUrl[0]}>{childArray[6].title}</a> {childArray[6].ups} upvotes</i>;
+                    var r8 = <i className="fa fa-thumbs-o-up">8: <a href= {permaUrl[0]}>{childArray[7].title}</a> {childArray[7].ups} upvotes</i>;
+                      var r9 = <i className="fa fa-thumbs-o-up">9: <a href= {permaUrl[0]}>{childArray[8].title}</a> {childArray[8].ups} upvotes</i>;
+                        var r10 = <i className="fa fa-thumbs-o-up">10: <a href= {permaUrl[0]}>{childArray[9].title}</a> {childArray[9].ups} upvotes</i>;
 
       base.setState({
         post1: r1,
